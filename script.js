98239243526d15cab3e8dfa06d1c61df08bfa207
SuphakitSaengsawang <thaibot636@gmail.com>
2026-03-27 12:32:51 +0700

    Got the square and everything right. Now i needed to accept input

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-// Source - https://stackoverflow.com/a/57550587
-// Posted by Nidhin Joseph, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-03-27, License - CC BY-SA 4.0
-
-function makeRows(rows, cols) {
+let gridSize = 16;
+function makeRows(gridSize) {
   const container = document.getElementById("grid-container");
-  // set variable value on container
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
   let cell;
-  for (let c = 0; c < (rows * cols); c++) {
+  for (let c = 0; c < (gridSize * gridSize); c++) {
     cell = document.createElement("div");
     cell.innerText = (c + 1);
+    cell.style.color = `white`;
+    cell.style.textAlign =`center`;
+    cell.style.backgroundColor = `brown`
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(16, 16);
+makeRows(gridSize);
+
+let boxSize = 960 / gridSize;
+
+function squareSize(boxSize) {
+  const squares = document.getElementsByClassName("grid-item");
+
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.width = boxSize + "px";
+    squares[i].style.height = boxSize + "px";
+  }
+}
+
+squareSize(boxSize);
